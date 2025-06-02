e1e3bbcede6afd3c8ee15b3e72bcfd55c63324da
urbanstech <marcos.urbanski@gmail.com>
2025-06-02 15:54:25 -0300

    alterado allowed hosts para dominios

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['curso-django-vmuk.onrender.com', 'localhost', '127.0.0.1', '0.0.0.0', '201.21.190.248']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
