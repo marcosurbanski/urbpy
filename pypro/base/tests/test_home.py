853fd1acd7b7feed9cd56488d58e851ee939e246
urbanstech <marcos.urbanski@gmail.com>
2025-06-15 12:43:36 -0300

    alterado alguns testes

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,11 +1,26 @@
 from django.test import Client
+from django.urls import reverse
 from pypro.django_assertions import assert_contains
+import pytest
 
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('home'))
+    return resp
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+def test_status_code(resp):   
     assert resp.status_code == 200
 
-def test_title(client: Client):
-    resp = client.get('/')
+def test_title(resp):    
     assert_contains(resp, '<title>UrbPy - Future IT course platform</title>')
+
+def test_home_link(resp):    
+    assert_contains(resp, 'content="Marcos Urbanski"')
+
+
+def test_description(resp):   
+    assert_contains(resp, 'content="Pagina da aplicação URBPY"')
+
+def test_author(resp):   
+    assert_contains(resp, 'href="/">UrbPy</a>')
+
--- pypro/urls.py
+++ pypro/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('home', home, name='home'),
 ]
 
 if settings.DEBUG:
