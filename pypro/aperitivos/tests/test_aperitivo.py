5d761275f285ec2f012affa00b6d8d5f8e86cbfd
urbanstech <marcos.urbanski@gmail.com>
2025-06-22 19:48:27 -0300

    Adicionado o video de motivação

--- pypro/aperitivos/tests/test_aperitivo.py
+++ pypro/aperitivos/tests/test_aperitivo.py
@@ -1,6 +1,6 @@
 import pytest
 from django.urls import reverse
-
+from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
@@ -9,3 +9,11 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe src="https://share.synthesia.io/embeds/videos/76c48edb-dd43-4415-809a-e1dc53efeb4c"')
