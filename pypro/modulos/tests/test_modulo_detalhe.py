b416c63be168628369d745a69703375cee21a03a
urbanstech <marcos.urbanski@gmail.com>
2025-07-09 17:20:53 -0300

    testes do modulo aula

--- pypro/modulos/tests/test_modulo_detalhe.py
+++ pypro/modulos/tests/test_modulo_detalhe.py
@@ -3,16 +3,19 @@ from django.urls import reverse
 from model_bakery import baker
 
 from pypro.django_assertions import assert_contains
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Aula, Modulo
 
 
 @pytest.fixture
 def modulo(db):
     return baker.make(Modulo)
 
+@pytest.fixture
+def modulo(modulo):
+    return baker.make(Aula, 3, modulo=modulo)
 
 @pytest.fixture
-def resp(client, modulo):
+def resp(client, modulo, aulas):
     resp = client.get(reverse('modulos:detalhe', kwargs={'slug': modulo.slug}))
     return resp
 
@@ -27,3 +30,8 @@ def test_descricao(resp, modulo: Modulo):
 
 def test_publico(resp, modulo: Modulo):
     assert_contains(resp, modulo.publico)
+
+
+def test_aulas_titulos(resp, aulas):
+    for aula in aulas:
+        assert_contains(resp, aula.titulo)
