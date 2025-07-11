ddeb56b152f6f06b3bcba5c98186d07bc689919b
urbanstech <marcos.urbanski@gmail.com>
2025-07-11 17:43:02 -0300

    adicionado os botoes de naavegação entre as aulas

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -11,4 +11,18 @@ def detalhe(request, slug):
 
 def aula(request, slug):
     aula = facade.encontrar_aula(slug)
-    return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
+    modulo = aula.modulo
+    aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
+    
+    index = aulas.index(aula)
+    aula_anterior = aulas[index - 1] if index > 0 else None
+    aula_proxima = aulas[index + 1] if index < len(aulas) - 1 else None
+
+    context = {
+        'aula': aula,
+        'modulo': modulo,
+        'aula_anterior': aula_anterior,
+        'aula_proxima': aula_proxima,
+    }
+
+    return render(request, 'modulos/aula_detalhe.html', context)
