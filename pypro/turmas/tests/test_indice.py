00dde1b78caf498d11258d80bab9f38e6fed874e
marcosurbanski <marcos.urbanski@gmail.com>
2025-07-15 15:54:37 -0300

    removido botao de contribuição

---
+++ pypro/turmas/tests/test_indice.py
@@ -0,0 +1,10 @@
+from django.urls import reverse
+import pytest
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('turmas: indice'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
