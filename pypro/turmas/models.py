9f23bf7c047c6c0551ef9c75e9e397af6e0c7372
marcosurbanski <marcos.urbanski@gmail.com>
2025-07-17 09:11:22 -0300

    test

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 
@@ -7,4 +7,6 @@ class Turma(models.Model):
     nome = models.CharField(max_length=64)
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
-    fim = models.DateField()
+    fim = models.DateField()    
+
+class Matricula(models.Model)
