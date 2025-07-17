c2b0b88888847509a9a002b6152d6e147ae533b5
marcosurbanski <marcos.urbanski@gmail.com>
2025-07-17 10:32:12 -0300

    Implementado Inlineadminmodel

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -4,8 +4,13 @@ from django.contrib import admin
 from pypro.turmas.models import Turma
 
 
+class MatriculaInline(admin.TabularInline):
+    model = Turma.alunos.through
+
+
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
+    inLines = [MatriculaInline]
     list_display = ('nome', 'slug', 'inicio', 'fim')
     prepopulated_fields = {'slug': ('nome',)}
     ordering = ('-inicio',)
