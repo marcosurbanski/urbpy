7d928e1c6697a2a1b1faffc757210e24d2c9e9e9
marcosurbanski <marcos.urbanski@gmail.com>
2025-07-23 11:45:35 -0300

    Remover arquivos teste_env.py e verifica_env.py

--- teste_env.py
+++
@@ -1,5 +0,0 @@
-from decouple import config
-from distutils.util import strtobool
-
-print("Raw value from .env:", config("COLLECTFAST_ENABLED", default="not found"))
-print("Parsed bool value   :", config("COLLECTFAST_ENABLED", cast=lambda v: bool(strtobool(v)), default=False))
